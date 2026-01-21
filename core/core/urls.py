82c698f8defa1d52514307bc74708bcb23a6e526
You Raj <yourajbhattarai@lumbini.edu.np>
2026-01-21 18:17:28 +0400

    path update

--- core/core/urls.py
+++ core/core/urls.py
@@ -26,7 +26,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    #path('api', include('payroll.urls')),
+    path('api', include('payroll.urls')),
 ]
 
 urlpatterns = [
