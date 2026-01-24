c959dfd9c180af25d37951f15fa7a1cd4bd28776
You Raj <yourajbhattarai@lumbini.edu.np>
2026-01-24 14:13:52 +0400

    adding userprofile

--- core/core/settings.py
+++ core/core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yx$lu8fx*h#7)3ms^$1(f@#iv9(0a0-dujw#^f7++&athau8%j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 
