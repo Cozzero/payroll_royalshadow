f4250e04798a69233bfa7a28a709de4ae93fb633
You Raj <yourajbhattarai@lumbini.edu.np>
2026-02-05 17:43:53 +0400

    updated app

--- nodeapp/app.jsx
+++ nodeapp/app.jsx
@@ -1,7 +1,18 @@
-function app() {
-    return (
-    <h1> ROYAL SHADOW SERVICES </h1>
-    );
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css'; // Import your CSS styles here
+import AppHeader from './components/AppHeader';
+import AppFooter from './components/AppFooter';
+import MainContent from './components/MainContent';
 
-export default app;
+const App = () => {
+    return (
+        <div className="app">
+            <AppHeader />
+            <MainContent />
+            <AppFooter />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
