c8e6f92666fb9e429e3a832c7b6ff073366d061a
You Raj <yourajbhattarai@lumbini.edu.np>
2026-02-06 15:38:01 +0400

    adding css and updating main

--- nodeapp/main.jsx
+++ nodeapp/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 import './index.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
+
+export default App;
+
+
